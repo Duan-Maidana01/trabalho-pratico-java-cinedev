15d7c5fc31602bf05dedc2d341148d5197620ae3
Duan e joão Piedade <duan.maidana@aluno.senai.br e joao.p.fernandes23@aluno.senai.br>
2025-09-17 14:50:57 -0300

    Terceiro commit
    Inicializando todos os assentos como livres

--- CineDev.java
+++ CineDev.java
@@ -1,18 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+public class CineDev {
 
-/**
- *
- * @author 474770
- */
-public class Principal {
+    // Matriz que representa a sala de cinema
+    static char[][] salaDeCinema = new char[10][20];
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        inicializarSala();
+
+        int opcao;
+
+        do {
+            exibirMenu();
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    exibirMapa();
+                    break;
+                case 2:
+                    comprarIngresso(scanner);
+                    break;
+                case 3:
+                    cancelarIngresso(scanner);
+                    break;
+                case 4:
+                    exibirRelatorio();
+                    break;
+                case 5:
+                    System.out.println("Encerrando o sistema. Até logo!");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+
+        } while (opcao != 5);
+
+        scanner.close();
     }
 }
+
+// Inicializa todos os assentos como livres "L"
+    public static void inicializarSala() {
+        for (int i = 0; i < salaDeCinema.length; i++) {
+            for (int j = 0; j < salaDeCinema[i].length; j++) {
+                salaDeCinema[i][j] = 'L';
+            }
+        }
+    }
+
+
